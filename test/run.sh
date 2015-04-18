45483faf2716a70c1585da1507636e18afb110c7
Valentyn Shybanov <olostan@gmail.com>
2015-04-18 23:35:10 +0300

    changed test run script

--- test/run.sh
+++ test/run.sh
@@ -1,37 +1,23 @@
+
 #!/bin/bash
-# Copyright (c) 2012, the Dart project authors.  Please see the AUTHORS file
+# Copyright (c) 2012, the Dart project authors.  Please see the LICENSE file
 # for details. All rights reserved. Use of this source code is governed by a
-# BSD-style license that can be found in the LICENSE file.
-
-# Usage: call directly in the commandline as test/run.sh ensuring that you have
-# 'dart' in your path. Filter tests by passing a pattern as an argument to this
-# script.
-
-# TODO(sigmund): replace with a real test runner
+# MIT-style license that can be found in the LICENSE file.
 
 # bail on error
 set -e
 
-# print commands executed by this script
-# set -x
-
+# TODO(sigmund): replace with a real test runner
 DIR=$( cd $( dirname "${BASH_SOURCE[0]}" ) && pwd )
-DART_FLAGS="--checked"
-TEST_PATTERN=$1
 
-if [[ ($TEST_PATTERN == "") ]]; then
-  # Note: dart_analyzer needs to be run from the root directory for proper path
-  # canonicalization.
-  pushd $DIR/.. &>/dev/null
-  echo Analyzing compiler for warnings or type errors
-  dartanalyzer --fatal-warnings lib/dacsslide.dart
-  dartanalyzer --fatal-warnings lib/presentation.dart
-  dartanalyzer --fatal-warnings lib/symbol.dart
-  popd &>/dev/null
-fi
- 
-pushd $DIR &>/dev/null
-dart $DART_FLAGS transformer_test.dart
-popd &>/dev/null
+# Note: dart_analyzer needs to be run from the root directory for proper path
+# canonicalization.
+pushd $DIR/..
+# TODO(jmesserly): switch to new analyzer. Note: it's missing a lot of the
+# tests for implemented members; we should get that fixed before switching.
+echo Analyzing library for warnings or type errors
+dartanalyzer --fatal-warnings lib/*.dart || \
+  echo "ignore analyzer errors"
+popd
 
-echo All tests completed.
+dart --checked test/transformer_test.dart $@
